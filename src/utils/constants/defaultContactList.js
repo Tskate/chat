1dbc2530a7aa7f53ea061a0db06c56eb9c42a844
Tskate <kate353@ukr.net>
2022-08-18 13:42:21 +0300

    feat: Add default user list and chat history list

---
+++ src/utils/constants/defaultContactList.js
@@ -0,0 +1,62 @@
+export const defaultContactList = [
+  {
+    id: "0101",
+    name: "Josefina",
+    icon: "https://peopleinmedia.org/wp-content/uploads/2020/07/annabelle-wallis-1-240x320.jpg",
+    status: 1,
+  },
+  {
+    id: "0102",
+    name: "Mark",
+    icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRk9elSH6oRviRtoadmMA0a2b30XESQLL2NQzi58h-bgy_cYYitPzl_sbg6Asi82dQxXk&usqp=CAU",
+    status: 1,
+  },
+  {
+    id: "0103",
+    name: "Ben Harris",
+    icon: "https://images.mubicdn.net/images/cast_member/693784/cache-422720-1553555632/image-w856.jpg?size=800x",
+    status: 1,
+  },
+  {
+    id: "0104",
+    name: "Linda Walker",
+    icon: "https://lifeglobe.net/x/entry/4683/penelopecruz1.jpg",
+    status: 1,
+  },
+  {
+    id: "0105",
+    name: "Liza",
+    icon: "https://modelnyeagentstva.com/sites/default/files/modelnoe-agentstvo/5-7/b12ee1e0b63a.jpg",
+    status: 1,
+  },
+  {
+    id: "0106",
+    name: "Tom Davis",
+    icon: "https://i1.wp.com/basetop.ru/wp-content/uploads/2018/05/jwhxz5qt.jpg",
+    status: 1,
+  },
+  {
+    id: "0107",
+    name: "Thomas",
+    icon: "https://feme.ua/wp-content/uploads/2020/03/Robert-Downey-Jr.jpg",
+    status: 1,
+  },
+  {
+    id: "0108",
+    name: "Emily",
+    icon: "https://img.joinfo.com/i/2019/09/5d7246ff7b8c2.jpg",
+    status: 1,
+  },
+  {
+    id: "0109",
+    name: "Liza Moore",
+    icon: "https://i.pinimg.com/originals/5c/f1/63/5cf16309942e13966398d3249d5f8a34.jpg",
+    status: 1,
+  },
+  {
+    id: "0110",
+    name: "Denis",
+    icon: "https://www.kleo.ru/img/articles/Z7Z1i.jpg",
+    status: 1,
+  },
+];
